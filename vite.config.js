e15bdfa9cdd5d4498a51834a8936faf6caff2302
linganaboinasushmitha <144676274+linganaboinasushmitha@users.noreply.github.com>
2025-11-24 11:02:48 +0530

    Updated deploy setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
+//import { defineConfig } from 'vite'
+//import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+//export default defineConfig({
+ // plugins: [react()],
+//})
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/3D-animation1/',   // <-- Correct GitHub repo name
   plugins: [react()],
 })
+
